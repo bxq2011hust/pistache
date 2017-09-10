dadcce8b6779fcc7711b63059c2e2635cdcb15f2
bxq2011hust <bxq2011hust@qq.com>
2017-09-10 15:00:36 +0800

    fix CTRL+C

--- src/common/os.cc
+++ src/common/os.cc
@@ -191,10 +191,7 @@ namespace Polling {
     Epoll::poll(std::vector<Event>& events, size_t maxEvents, std::chrono::milliseconds timeout) const {
         struct epoll_event evs[Const::MaxEvents];
 
-        int ready_fds = -1;
-        do {
-            ready_fds = epoll_wait(epoll_fd, evs, maxEvents, timeout.count());
-        } while (ready_fds < 0 && errno == EINTR);
+        int ready_fds = epoll_wait(epoll_fd, evs, maxEvents, timeout.count());
 
         if (ready_fds > 0) {
             for (int i = 0; i < ready_fds; ++i) {
